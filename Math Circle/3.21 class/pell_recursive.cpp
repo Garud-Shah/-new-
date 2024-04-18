be27f3c9622b9bb92ca2ebd5fccf356c93814baa
Garud Shah <garudshah1@gmail.com>
2024-04-18 16:07:46 -0700

    .

--- Math Circle/3.21 class/pell_recursive.cpp
+++ Math Circle/3.21 class/pell_recursive.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int main(){
     int a = 1; 
     int b = 0;
-    for (int i=0; i<20; i++){
+    for (int i=0; i<40; i++){
         cout << a << " " << b << endl;
         a = a + 2*b;
         b = a - b;
--- Math Circle/4.18 class/pell.cpp
+++ Math Circle/4.18 class/pell.cpp
@@ -1,13 +1,38 @@
 #include <iostream>
 #include <cmath>
 using namespace std;
+ostream&
+operator<<( ostream& dest, __int128_t value )
+{
+    ostream::sentry s( dest );
+    if ( s ) {
+        __uint128_t tmp = value < 0 ? -value : value;
+        char buffer[ 128 ];
+        char* d = end( buffer );
+        do
+        {
+            -- d;
+            *d = "0123456789"[ tmp % 10 ];
+            tmp /= 10;
+        } while ( tmp != 0 );
+        if ( value < 0 ) {
+            -- d;
+            *d = '-';
+        }
+        int len = end( buffer ) - d;
+        if ( dest.rdbuf()->sputn( d, len ) != len ) {
+            dest.setstate( ios_base::badbit );
+        }
+    }
+    return dest;
+}
 int main(){
-    int kmax = 1'000'000'000;
-    int k = 1;
-    long long int x = 2;
-    int amin = 0; int bmin = 0;
-    for (long long int a=amin; a<kmax; a++){
-        for (long long int b=(a / sqrt(x) - 1); b<=(a / sqrt(x) + 1); b++){
+    __int128_t kmax = 1'000'000'000;
+    __int128_t k = 1;
+    __int128_t x = 2;
+    __int128_t amin = 0; __int128_t bmin = 0;
+    for (__int128_t a=amin; a<kmax; a++){
+        for (__int128_t b=(a / sqrt(x) - 1); b<=(a / sqrt(x) + 1); b++){
             if (abs(a*a - x*b*b) == 1){
                 cout << a << " " << b << endl;
                 amin = a;
